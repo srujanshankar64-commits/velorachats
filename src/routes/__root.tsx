f0b3dc1d6a132feea503f915abf887f7a2cd98fe
Shankar Pattar <srujanshankar64@gmail.com>
2026-05-25 11:11:05 +0530

    Update Monetag script source to use HTTPS

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -83,7 +83,7 @@ function RootShell({ children }: { children: React.ReactNode }) {
         {/* Monetag Vignette Banner */}
         <script src="https://n6wxm.com/vignette.min.js" defer data-zone="11052131"></script>
         {/* Monetag In-Page Push */}
-       <script src="//ssp.monetag.com/tag/11052181" async></script>
+    <script src="https://ssp.monetag.com/tag/11052181" async></script>
         {/* Cloudflare Web Analytics */}
         <script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "118112b5573b4eadbd1f78c0e043f479"}'></script>
       </head>
@@ -94,7 +94,6 @@ function RootShell({ children }: { children: React.ReactNode }) {
     </html>
   );
 }
-
 function RootComponent() {
   const { queryClient } = Route.useRouteContext();
   return (
