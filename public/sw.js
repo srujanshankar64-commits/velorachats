2ebcc9e51595aadff0076b8ebc25607d509e3ef6
srujanshankar64-commits <your-github-email@example.com>
2026-05-28 16:07:45 +0530

    add monetag push notification service worker

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11052033
+    "zoneId": 11068665
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
