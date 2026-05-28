25ef89601d336e8ac21bc1757788e229323e0dde
srujanshankar64-commits <your-github-email@example.com>
2026-05-28 22:08:25 +0530

    build: clean up route compilation references

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -1,3 +1,7 @@
+/* eslint-disable */
+// @ts-nocheck
+// noinspection JSUnusedGlobalSymbols
+
 import { Route as rootRoute } from './routes/__root'
 import { Route as AuthenticatedImport } from './routes/_authenticated'
 
@@ -6,6 +10,14 @@ const AuthenticatedRoute = AuthenticatedImport.update({
   getParentRoute: () => rootRoute,
 } as any)
 
+const AuthenticatedMessagesUserIdRoute = AuthenticatedRoute.update({
+  path: '/messages/$userId',
+  id: '/messages/$userId',
+  getParentRoute: () => AuthenticatedRoute,
+} as any)
+
 export const routeTree = rootRoute.addChildren([
-  AuthenticatedRoute,
+  AuthenticatedRoute.addChildren([
+    AuthenticatedMessagesUserIdRoute,
+  ]),
 ])
